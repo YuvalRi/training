c082e9fd0351de3f9221812fed9f4a8638265484
YuvalRi <yuvalritsker@gmail.com>
2021-09-25 02:23:46 +0300

    ex1_fixed3

--- ex1.py
+++ ex1.py
@@ -2,11 +2,8 @@
 import math
 
 def area(r):
-    return((math.pi*r**2))
+    return math.pi*r**2 
 
 if __name__ == "__main__":
  r = float(input("please enter a value: "))
  print(f"The answer is:{area(r)}")
-
-
-
