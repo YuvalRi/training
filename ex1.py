2fcf849caa21758dd6488c6f266656583296c66b
YuvalRi <yuvalritsker@gmail.com>
2021-09-25 02:02:56 +0300

    ex1_fixed

--- ex1.py
+++ ex1.py
@@ -1,9 +1,9 @@
 #area of a circle
 import math
-def Area(r):
-    return((math.pi*r**2))
 
 if __name__ == "__main__":
-    ar = str(Area(1.1))
-    print(f"The answer is:{ar}")
-    
+ r = float(input("please enter a value: "))
+ print(f"The answer is:{math.pi*r**2}")
+
+
+
