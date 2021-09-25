a5efafefd511a2a73ef688c6875b1e78fd521817
YuvalRi <yuvalritsker@gmail.com>
2021-09-25 17:51:15 +0300

    ex2

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-#fixed
+#reversed user's first and last name
+import re
+
+def rev(x):
+    st = x.split(" ")
+    return(" ".join(reversed(st)))
+    
+if __name__ == "__main__":
+    x = input("please enter your first name and last name: ")
+    print(rev(x))
