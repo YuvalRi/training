cf3526e95ef8fd7773f4c60844b75e0cc2d831ca
YuvalRi <yuvalritsker@gmail.com>
2021-10-02 17:51:44 +0300

    ex3_1

---
+++ ex3.py
@@ -0,0 +1,6 @@
+#The first and last element in the following list 
+color_list = ["Red", "Green", "White", "Black"]
+print(color_list[0:4:3])
+
+n = len(color_list)
+print("The first element is: ",color_list[0] ,"The last element is: ", color_list[n-1])
