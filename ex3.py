c1c2f267a0ea4167d5e799bdc283a15c545cac6d
YuvalRi <yuvalritsker@gmail.com>
2021-10-02 18:05:30 +0300

    printing output with 2 lines

--- ex3.py
+++ ex3.py
@@ -1,6 +1,4 @@
 #The first and last element in the following list 
 color_list = ["Red", "Green", "White", "Black"]
-print(color_list[0:4:3])
-
 n = len(color_list)
-print("The first element is: ",color_list[0] ,"The last element is: ", color_list[n-1])
+print("The first element is: ", color_list[0], "\nThe last element is: ", color_list[n-1])
