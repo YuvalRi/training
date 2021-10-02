8a3378f404e34ae36051d404a4cfee93554ca83d
YuvalRi <yuvalritsker@gmail.com>
2021-10-02 19:14:10 +0300

    first try in ex5

---
+++ ex5.py
@@ -0,0 +1,10 @@
+#sum of three given numbers, if they are equal then we will get three times of their sum 
+if __name__ == "__main__":
+    x = int(input("please insert a number: "))
+    y = int(input("please insert a number: "))
+    z = int(input("please insert a number: "))
+
+if x == y == z:
+    print(3*(x+y+z))
+else:
+    print(x+y+z)
