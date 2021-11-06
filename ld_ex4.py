b25a934d876a155133c22d9a0226106662d37e11
YuvalRi <yuvalritsker@gmail.com>
2021-11-06 21:47:39 +0200

    ex4_second try

--- ld_ex4.py
+++ ld_ex4.py
@@ -1,8 +1,20 @@
 #exercise 4 - concatenating following dictionaries to create a new one
+
+def taskA(A,B):
+    for key in B:
+        A[key] = B[key]
+    return A
+
+def taskB(A,B):
+    for key in B:
+        if key in A.keys():
+            lst = [A[key], B[key]]
+            A[key] = lst
+        else:
+            A[key] = B[key]
+    return A
+
 if __name__ == "__main__":
-    dic1 = {1:10, 2:20} #sample dictionary
-    dic2 = {3:30, 4:40}
-    dic3 = {5:50, 6:60}
-    new_dict = dic1.update(dic2)
-    new_dict2 = dic1.update(dic3)
-    print(dic1)
+    A = {1:20, 2:50}
+    B = {1:25, 3:30}
+    print(taskB(A,B))
