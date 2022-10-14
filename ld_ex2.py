2cc15044bb72296eb93889b671404d36b8725558
YuvalRi <yuvalritsker@gmail.com>
2022-10-14 17:41:51 +0300

    ex 2 completed

--- ld_ex2.py
+++ ld_ex2.py
@@ -4,12 +4,15 @@ def smallestElement(lst):
     '''
     function that recieves a list of integers and returns its smallest element
     '''
-    smallest = lst[0]
-    for i in lst:
-        if i < smallest:
-            smallest = i
-    return smallest
+    if len(lst) == 0:
+        print("Error. Please enter at least one element in the list")
+    else:
+        smallest = lst[0]
+        for i in lst:
+            if i < smallest:
+                smallest = i
+        return smallest
 
 
 if __name__ == "__main__":
-    print(smallestElement([-1, -5, -99, 100, -1500]))
+    print(smallestElement([]))
