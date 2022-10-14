d6d46dc9cfba52f491caffa0f210fd8d57c16bee
YuvalRi <yuvalritsker@gmail.com>
2022-10-14 13:43:29 +0300

    ex6 completed

---
+++ EvenorOdd.py
@@ -0,0 +1,8 @@
+# ex6
+
+if __name__ == "__main__":
+    x = int(input("Please enter a number: "))
+    if x % 2 == 0:
+        print("The given number is even")
+    else:
+        print("The given number is odd")
