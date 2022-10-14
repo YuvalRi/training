30666ddcb484b145f36652bd73159f20fb783558
YuvalRi <yuvalritsker@gmail.com>
2022-10-14 16:21:50 +0300

    ex1 completed

--- ld_ex1.py
+++ ld_ex1.py
@@ -1,10 +1,14 @@
-#program to sum all elements in a list
+# ex1
+
+def elementSum(lst: list):
+    '''
+    function that recieves a list and returns the sum of its elements
+    '''
+    lst_sum = 0
+    for i in range(len(lst)):
+        lst_sum = lst[i] + lst_sum
+    return lst_sum
+
+
 if __name__ == "__main__":
-    lst = [] #defining empty list which contain the elements the user will enter, then we want to sum all elements in this list.
-    x = int(input("Please enter number of elements: ")) #total number of elements in our cute list
-
-    for i in range(x): #we want to insert an element for each index in the list. if x equal to 2, we want the user to insert 2 elemnts.
-        e = int(input("Enter element:")) #we want that each element will be a type of integer
-        lst.append(e) #adding each element (e) to the our list (lst)
-
-    print("Sum of elements in the given list is: ", sum(lst)) #required output
+    print(elementSum([1, 2]))
