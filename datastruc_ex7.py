c2d027212a2e3fd7865122bb55e48a4730fb2c47
YuvalRi <yuvalritsker@gmail.com>
2022-10-16 01:21:58 +0300

    ex 7 completed

--- datastruc_ex7.py
+++ datastruc_ex7.py
@@ -13,18 +13,14 @@ def func(n: int, random: bool, lst: list):
             selected_words = []
             for i in range(len(randomlist)):
                 selected_words.append(text_list[randomlist[i]])
-            print(selected_words)
+            return selected_words
         else:
             text_list = allText.split('\n')
             selected_words = []
             for i in range(len(lst)):
                 selected_words.append(text_list[lst[i]])
-            print(selected_words)
+            return selected_words
 
-
-if __name__ == "__main__":
-    func(7, True, lst=[1, 2, 3, 4, 5, 6, 7])
-    func(5, False, lst=[1, 2, 3, 4, 5])
 
 # b
 
@@ -41,7 +37,43 @@ def freqDict(s: str):
     return all_freq
 
 
+def freqFunc(lst: list):
+    '''
+    A function which convert list of strings to one string
+    '''
+    new_string = "".join(lst)
+    return new_string
+
+
+def maxCharacter(dict: dict):
+    '''
+    A function which returns the maximum value in a given dictionary
+    '''
+    max_value = max(dict, key=dict.get)
+    return max_value
+
+
+def findMedian(dict: dict):
+    new_lst = sorted(dict.values())
+    if (len(new_lst)) % 2 == 0:
+        med1 = new_lst[(len(new_lst))//2]
+        med2 = new_lst[(len(new_lst))//2 - 1]
+        med = (med1 + med2)/2
+        return med
+    else:
+        med = new_lst[(len(new_lst))//2]
+        return med
+
+
 if __name__ == "__main__":
-    print(freqDict("yuvalritsker"))
-    print(freqDict("ofekshmuel"))
-    print(freqDict("chugokujin"))
+    # in this case random = True
+    a = freqDict(freqFunc(func(7, True, lst=[1, 2, 3, 4, 5, 6, 7])))
+    print(a)
+    print(maxCharacter(a))
+    print(findMedian(a))
+    # in this case random = False
+    b = freqDict(freqFunc(func(5, False, lst=[1, 2, 3, 4, 5])))
+    print(b)
+    print(maxCharacter(b))
+    print(findMedian(b))
+
