623738015db3f1ce049f19fc7e84c76a81cb7d58
YuvalRi <yuvalritsker@gmail.com>
2022-10-18 17:52:24 +0300

    ex9 completed

---
+++ datastruc_ex9.py
@@ -0,0 +1,26 @@
+# ex 9
+
+
+from os import lstat
+
+
+def dimenstionaList(m: int, n: int):
+    '''
+    A function which recieves 2 integers
+    and returns a 2-dimensional list of size mXn
+    '''
+    list1 = []
+    lists = []
+    for i in range(m):
+        lists.append(list1)
+    for i in range(m):
+        for j in range(m):
+            lists[j] = [i*j for i in range(n)]
+    return lists
+
+
+if __name__ == "__main__":
+    print(dimenstionaList(4, 5), end=" \n")
+    #dimenstionaList(4, 4)
+    #dimenstionaList(2, 3)
+
