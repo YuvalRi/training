c9b616b0e7ecee5b14b08d2a73af456c303c4930
YuvalRi <yuvalritsker@gmail.com>
2022-10-19 10:46:58 +0300

    ex2 changing iteartion variable name

--- datastruc_ex2.py
+++ datastruc_ex2.py
@@ -2,15 +2,15 @@
 
 def smallestElement(lst):
     '''
-    function that recieves a list of integers and returns its smallest element
+    A function that recieves a list of integers and returns its smallest element
     '''
     if len(lst) == 0:
         print("Error. Please enter at least one element in the list")
     else:
         smallest = lst[0]
-        for i in lst:
-            if i < smallest:
-                smallest = i
+        for ele in lst:
+            if ele < smallest:
+                smallest = ele
         return smallest
 
 
