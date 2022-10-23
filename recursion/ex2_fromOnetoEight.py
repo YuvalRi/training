7bc0b1a299449ba6d289c58bdec1005c3c48452f
YuvalRi <yuvalritsker@gmail.com>
2022-10-23 11:28:52 +0300

    ex2 completed

---
+++ recursion/ex2_fromOnetoEight.py
@@ -0,0 +1,18 @@
+# ex 2
+
+def naturalNumbers_backward(n: int):
+    '''
+    A recursive function prints first n natural numbers
+    '''
+    if n < 1:
+        print("Error: please choose a natural number.")
+    elif n == 1:
+        print(n, end = ", ")
+    else:
+        naturalNumbers_backward(n-1)
+        print(n, end = ", ")
+
+    
+if __name__ == "__main__":
+    n = int(input("Please enter a natural number: "))
+    naturalNumbers_backward(n)
