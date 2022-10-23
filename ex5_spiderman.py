5cffc7ebbee46a0f3858ce8139b02e4ec5fcf15c
YuvalRi <yuvalritsker@gmail.com>
2022-10-23 14:03:13 +0300

    ex5 completed

---
+++ ex5_spiderman.py
@@ -0,0 +1,30 @@
+# ex 5 
+
+def spidermanSteps(n: int):
+    '''
+    A recursive function which returns how many ways
+    spiderman can climb a building
+    '''
+    if n < 1:
+        print("Error! Please enter a natural number.")
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return spidermanSteps(n-2) + spidermanSteps(n-1)
+
+if __name__ == "__main__":
+    # we can see that from building of height 3, the ways equal to the sum 
+    # of the 2 previous option of height (as Fibonacci sequence)
+    print(spidermanSteps(1))
+    print(spidermanSteps(2))
+    print(spidermanSteps(3))
+    print(spidermanSteps(4))
+    print(spidermanSteps(5))
+    print(spidermanSteps(6))
+
+
+
+
+
