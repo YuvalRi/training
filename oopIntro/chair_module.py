de3906f5ab92a45017e87383d68e1e03be39a62f
YuvalRi <yuvalritsker@gmail.com>
2022-10-26 23:20:56 +0300

    exA and B works

---
+++ oopIntro/chair_module.py
@@ -0,0 +1,68 @@
+import time
+from ShowFunc import show
+
+# A
+
+class Gun:
+
+    # The constructor function
+    def __init__(self, ammunition: int, model: str):
+        if ammunition < 0 or ammunition > 6:
+            print("Error! Please enter a number between [0,6]")
+        else:
+            self.ammunition = ammunition
+        if not (model == "Sniper" or model == "Assault" or model == "Automatic"):
+            print("Error! Please enter a valid type of gun")
+        else:
+            self.model = model
+  
+    def reload_magazine(self):
+        '''
+        Reload ammunition to its max capacity
+        '''
+        self.ammunition = 6
+        return self.ammunition
+
+    def get_ammu(self) -> int:
+        '''
+        Returns the current ammunition count
+        '''
+        return self.ammunition
+
+    def get_model(self) -> str:
+        '''
+        Returns the gun model
+        '''
+        return self.model
+
+    def is_empty(self):
+        '''
+        Returns True if ammunition is empty and false otherwise
+        '''
+        if self.ammunition == 0:
+            return True
+        else:
+            return False
+   
+    def shoot(self):
+        '''
+        A function which cause the gun to shoot
+        '''
+        if(self.ammunition != 0):
+            show(self)
+            self.ammunition -= 1
+        else:
+            print("Error! Please enter a number between [1,6]")
+
+# B
+
+if __name__ == "__main__":
+    g = Gun(ammunition=0, model="Sniper") # Creating a Gun of type "Sniper" with 0 ammunition
+    g.shoot() # Trying shooting it
+    print(g.is_empty()) # Checking it is has any ammunition left, print the answer
+    g.reload_magazine() # Reloading it
+    print(g.is_empty()) # Checking if it has ammunition again, print the answer
+    g.shoot() # Shooting it
+    # Creating a gun with invalid inputs
+    g1 = Gun(ammunition=8, model="Tesla")
+    g1.shoot()
