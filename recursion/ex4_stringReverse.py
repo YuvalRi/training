612130aa54930f980592ac8bec6c177b2bdd1693
YuvalRi <yuvalritsker@gmail.com>
2022-10-28 12:58:14 +0300

    pr last change

--- recursion/ex4_stringReverse.py
+++ recursion/ex4_stringReverse.py
@@ -5,12 +5,10 @@ def string_reverse(str: str):
     A recursive function which reverses a string
     '''
     if len(str) == 0:
-        print("Error! please enter at least one character.")
-    elif len(str) == 1:
         return str
     else: 
-        return string_reverse(str[1:]) + str[0]
+        return str[0] + string_reverse(str[1:])
 
 if __name__ == "__main__":
-    str = str(input("Please enter a string: "))
+    str = input("Please enter a string: ")
     print(string_reverse(str))
